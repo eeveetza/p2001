e3ba2dfaa3874e5d60a3384f0f9cdf2515f0ade4
eeveetza <ivica.stevanovic@bakom.admin.ch>
2022-02-15 13:27:26 +0100

    P2001 v4.0

--- matlab/compare_results_vs_ref.m
+++ matlab/compare_results_vs_ref.m
@@ -8,16 +8,19 @@ close all
 clc
 
 
-s = pwd;
-if ~exist('p838.m','file')
-    addpath([s '/src/'])
-end
+% s = pwd;
+% if (~exist('p838.m','file') )
+%     addpath([s '/private/'])
+%    fprintf(1,'octave\n');
+%end
 
 if (isOctave)
     pkg load windows;
     pkg load io;
     page_screen_output(0);
     page_output_immediately(1);
+
+    
 end
 
 filename_ofcom{1} = './validation_results/Validation examples ITU-R P.2001 b2iseac OFCOM.xlsx';
@@ -26,16 +29,34 @@ filename_ref{1} = './validation_examples/Validation examples ITU-R P.2001 b2isea
 filename_ref{2} = './validation_examples/Validation examples ITU-R P.2001 prof4.xlsx';
 
 for fln = 1:2
+  
+    if (isOctave)
+        xlsr1 = xlsopen(filename_ofcom{fln}, 0);
+        xlsr2 = xlsopen(filename_ref{fln},0);
+    end
     fprintf(1,'\n#Validation example: %s\n', filename_ofcom{fln});
     for pgn = 1:5
-        column = 'CA:CA'; %CA is for the final result Lb
+        column = 'CA2:CA444'; %CA is for the final result Lb
         pagenumber = ['Page' num2str(pgn)];
         
-        num_ofcom = xlsread(filename_ofcom{fln}, pagenumber, column);
-        num_ref = xlsread(filename_ref{fln}, pagenumber, column);
+        if (isOctave)
+
+            [num_ofcom_raw, xlsr1, rstatus ] = xls2oct (xlsr1, pagenumber, column);
+            [num_ref_raw, xlsr2, rstatus ] = xls2oct (xlsr2, pagenumber, column);
+            num_ofcom = parsecell(num_ofcom_raw);
+            num_ref = parsecell(num_ref_raw);
+        else
+            num_ofcom = xlsread(filename_ofcom{fln}, pagenumber, column);
+            num_ref = xlsread(filename_ref{fln}, pagenumber, column);
+        end
         
         maxdelta = max(abs(num_ofcom-num_ref));
         fprintf(1,'Maximum difference from the reference results in Lb on %s is: %g dB\n', pagenumber, maxdelta);
         
+    end
+    
+    if (isOctave)
+        xls1 = xlsclose(xlsr1);
+        xls2 = xlsclose(xlsr2);
     end
 end
--- matlab/private/tl_free_space.m
+++ matlab/private/tl_free_space.m
@@ -1,11 +1,11 @@
 function Lbfs = tl_free_space(f,d)
 %tl_free_space Free-space basic transmission loss 
 %   This function computes free-space basic transmission loss in dB
-%   as defined in ITU-R P.2001-2 Section 3.11
+%   as defined in ITU-R P.2001-4 Section 3.11
 %
 %     Input parameters:
 %     f       -   Frequency (GHz)
-%     d       -   Distance (km)
+%     d       -   3D Distance (km)
 %
 %     Output parameters:
 %     Lbfs    -   Free-space basic transmission loss (dB)
@@ -14,8 +14,9 @@ function Lbfs = tl_free_space(f,d)
 %     Rev   Date        Author                          Description
 %     -------------------------------------------------------------------------------
 %     v0    13JUL16     Ivica Stevanovic, OFCOM         Initial version
+%     v1    11FEB22     Ivica Stevanovic, OFCOM         modify the additive factor 92.44 -1 --> 92.4
 
-Lbfs = 92.44 + 20*log10(f) + 20*log10(d);
+Lbfs = 92.4 + 20*log10(f) + 20*log10(d);
 
 return
 end
